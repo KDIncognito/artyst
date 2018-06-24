ef5ec9cc3f7f7365f8c47428de6f6cf5dfa67139
sreekrishnadutta <sreekrishnadutta@gmail.com>
2018-06-24 18:17:18 +0530

    email.js
    new version uploaded

--- assets/js/email.js
+++ assets/js/email.js
@@ -60,3 +60,18 @@ return false;
 return true;
 }
 }
+
+var $form = $('form#form_id'),
+    url = 'https://script.google.com/macros/s/AKfycbxOgreutG-HW8QtjWhME92_sUSRcyUDc9MVCo7RBTG73dC9BtY/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
